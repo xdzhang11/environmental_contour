86ddde5cab8172faf8be8f27dd6d27ed6c41deaa
Xiaodong Zhang <xdotzhang@hotmail.com>
2025-01-26 14:38:31 +0800

    add gmm train file

---
+++ matlab/main_gmm_train.m
@@ -0,0 +1,42 @@
+% Train gmm with written and also inline codes
+clear;
+clc;
+addpath('../../../gmm');
+
+% Define the filename as a variable
+filename = 'onshore_detrend_150_180.mat';  % Data file to load
+model_filename = strrep(filename, '.mat', '_model.mat');  % Modify filename for saving model
+
+% Gaussian mixture model using matlab function
+%% load data x1 and x2
+f = fullfile('../../../data/hovsore_wind/data',filename);
+calmstd = load(f).calmstd;
+x1 = calmstd.u;
+x2 = calmstd.u_stdv;
+X(:,1) = x1;
+X(:,2) = x2;
+
+%% GMM training
+maxComponent = 8; % number of components
+
+% code written
+gmModel = gmm_train(X, maxComponent);
+
+
+% %% MATLAB inline code
+% options = statset('Display','final','MaxIter', 1000);
+% AIC = zeros(1,maxComponents);
+% gm = cell(1,maxComponents);
+% for k = 1:maxComponents
+%     gm{k} = fitgmdist(X,k,'Options',options, 'Replicates', 3);
+%     AIC(k)= gm{k}.AIC;
+% end
+% [minAIC,numComponents] = min(AIC);
+% gmModel = gm{numComponents};
+% 
+% % Generate new points
+% Xp = random(gmModel, 1e8);
+% x_i = Xp(:,1);
+% x_j = Xp(:,2);
+
+save(fullfile('../res', model_filename),'gmModel');
